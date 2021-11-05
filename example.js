c565a00496d2fc06df71a02cb59822d3a3950948
cala <cezara.christian@student.upt.ro>
2021-11-05 21:12:04 +0200

    added in example.js

--- example.js
+++ example.js
@@ -5,7 +5,6 @@ function showName(firstName)
 showName("cala");
 // cand vreti sa va ganditi la ce e aia null, ce e aia undefined
 // ganditi-va la memeu ala cu hartie igienica
-// webstorm mananca cam mult :(
 
 function showName1(firstName, ...args) //args o sa fie o lista cu
     // toate celelalte valori pe care le dai ca paramentri => omgomgomgomgomgm
@@ -39,7 +38,16 @@ let myArray = [1, 3, 5, 2, 4, 1, 6];
 // //ia uite la inca un exemplu care este foarte valid
 // const y = param1 => param1*2
 
-console.log(myArray.find(value => value %2 === 0))
-myArray.forEach((value:T, index: number) => {
-    console.log(value, index)
-});
+// ---------------------------------------
+
+const dictionary = {
+    one: 1,
+    two: "2",
+    "my-name": "Cala", //daca vrei proprietati cu cratima,
+        // trebuie cu ghilimele
+    3: "three"
+}
+
+console.log(dictionary["my-name"])
+console.log(dictionary.one)
+console.log(dictionary["3"])
