9b25c112e177b7df34c49a53550999e99a409b73
Raphael <raphaelaugustinov@gmail.com>
2021-11-05 21:13:03 +0200

    modified rapha.js

--- rapha.js
+++ rapha.js
@@ -3,4 +3,13 @@ function showName(firstname,lastname)
     console.log(firstname+ " " + lastname);
 }
 
-showName("raphael","augustinov);
+showName("raphael","augustinov");
+
+const dic = {
+    one:1,
+    two:2,
+    3:"trei"
+}
+console.log(dic.one)
+console.log(dic.two)
+console.log(dic[3])
