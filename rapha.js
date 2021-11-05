3c6084ac4cc81270b747065f7facfde699998aba
Raphael <raphaelaugustinov@gmail.com>
2021-11-05 21:20:50 +0200

    added set

--- rapha.js
+++ rapha.js
@@ -13,3 +13,12 @@ const dic = {
 console.log(dic.one)
 console.log(dic.two)
 console.log(dic[3])
+
+const mySet = new Set([1,2,4,8,3,4,6,4]);
+console.log(mySet)
+mySet.forEach(value => console.log(value));
+mySet.add(13);
+mySet.delete(4);
+console.log(mySet);
+mySet.clear();
+console.log(mySet);
