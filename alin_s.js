6146e6c5d919d27224afe7fb2b6d422daa9e233d
alin-sokolov <alinsokolov@gmail.com>
2021-11-05 20:43:46 +0200

    AS work

---
+++ alin_s.js
@@ -0,0 +1,19 @@
+function showName(firstname, lastname = 'Socolov'){
+    console.log(firstname + ' ' +lastname);
+}
+
+// ...args -> numar variabil de argumente
+function showNames(firstname, ...args){
+    console.log(firstname);
+    console.log(args);
+}
+
+
+const showMyName = function (firstName) {
+    console.log(firstName);
+}
+
+myArr = [1,4,2,4,1,3,32]
+
+
+
