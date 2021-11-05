b74fe99acc6099d67cf5ec9de20fd5dd56447006
alin-sokolov <alinsokolov@gmail.com>
2021-11-05 21:10:50 +0200

    added dictionary

--- alin_s.js
+++ alin_s.js
@@ -15,5 +15,25 @@ const showMyName = function (firstName) {
 
 myArr = [1,4,2,4,1,3,32]
 
+const dictionary = {
+    one: 1,
+    two: "2",
+    "my-name": 'Alin',
+    3: "three"
+}
+
+console.log(dictionary.one)
+console.log(dictionary["my-name"])
+console.log(dictionary["3"])
+console.log(dictionary[3])
+dictionary[3] = "alt string"
+console.log(dictionary[3])
+dictionary["cheie"] = "valoare"
+
+
+for( const key in dictionary){
+    console.log(key, dictionary[key])
+}
+
 
 
